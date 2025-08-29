eeffff0c8455c8cb5bc5b4ff9d98871b40963025
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-08-29 20:56:15 +0300

    modified Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import HomePage from "../pages/HomePage";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar" >
       <h1>SwapIt</h1>
+      <HomePage />
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/add-item" className="ml-4">
+          Add Item
+        </Link>
+      </div>
     </nav>
   );
 }
