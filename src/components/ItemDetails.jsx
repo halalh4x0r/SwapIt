a45e6a2b0885ac4612813be554d1a85856b2ffe1
Brian <brianmunyoki168@gmail.com>
2025-08-30 20:19:30 +0300

    updating itemdetails

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import ItemCard from "./ItemCard";
-import { useEffect, useState} from "react";
-function ItemDetails(){
-    const [items,setItems]=useState([])
-    useEffect(()=>{
-            fetch("http://localhost:3000/items")//fetched on dbjsons
-            .then((res)=>{
-                if(!res.ok) {
-                    throw new Error('Error!');
-                };
-                return res.json();///data passed
-            })
-            .then(data=>{
-                setItems(data)/// we setting the state item  state with new data from the server. it replaces the whole state now items is "data" from the server
-            })
-            .catch (error=>{
-                console.error("Fetch error:,error")
-            })
-        },[])// ensures t that this effect only runs once when the component mounts(its the actual syntax)
-        //kindly note on the details we have more information about the item in the description
-    return(
-        <>
-        {items.map((item)=>(
-            <div key={item.id} >
-                <img src={item.image}/>
-                <h3>Title:{item.title}</h3>
-                <p>Category:{item.category}</p>  
-                <p>Category:{item.description}</p> 
-            </div>
-        ))}
-        </>
-    )
-}
-
-export default ItemDetails;
-=======
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
@@ -87,4 +50,4 @@ function ItemDetails() {
 }
 
 export default ItemDetails;
->>>>>>> d93b9cb7c788d95384d38fa0f6c30eda17f18b88
+
